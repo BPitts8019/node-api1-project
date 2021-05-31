afa92b56966d717408e582b924606354237971fd
BPitts8019 <bradleyj.pitts@gmail.com>
2021-05-30 18:00:54 -0700

    Create a basic server with a root endpoint

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require("express");
+const server = express();
+const port = 8080;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+   res.send({ message: "Welcome to the Node API-1 project" });
+});
+
+server.listen(port, () => {
+   console.log(`Server is running on port: ${port}`);
+});
