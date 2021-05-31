29d2917fe826cf3929bfcbd2b255b152a6333dcc
BPitts8019 <bradleyj.pitts@gmail.com>
2021-05-30 19:27:48 -0700

    Surround async/await operations in try/catch blocks

--- index.js
+++ index.js
@@ -20,47 +20,50 @@ app.post(usersRoute, async (req, res) => {
       return;
    }
 
-   const { id } = await db.insert({
-      name: req.body.name,
-      bio: req.body.bio,
-   });
-   const user = await db.findById(id);
-
-   if (user) {
+   try {
+      const { id } = await db.insert({
+         name: req.body.name,
+         bio: req.body.bio,
+      });
+      const user = await db.findById(id);
       res.status(201).send(user);
-      return;
+   } catch (error) {
+      res.status(500).send({
+         message: "Something went wrong while creating the new user.",
+      });
    }
-
-   res.status(500).send({
-      message: "Something went wrong while creating the new user.",
-   });
 });
 
 app.get(usersRoute, async (req, res) => {
-   const users = await db.find();
-
-   if (users) {
+   try {
+      const users = await db.find();
       res.send(users);
-      return;
+   } catch (error) {
+      res.status(500).send({
+         errorMessage: "The users information could not be retrieved.",
+      });
    }
-
-   res.status(500).send({
-      errorMessage: "The users information could not be retrieved.",
-   });
 });
 
 app.get(`${usersRoute}/:id`, async (req, res) => {
    const { id } = req.params;
-   const user = await db.findById(id);
 
-   if (user) {
-      res.send(user);
-      return;
+   try {
+      const user = await db.findById(id);
+
+      if (user) {
+         res.send(user);
+         return;
+      }
+
+      res.status(404).send({
+         message: "The user with the specified ID does not exist.",
+      });
+   } catch (error) {
+      res.status(500).send({
+         message: "The user information could not be retrieved.",
+      });
    }
-
-   res.status(404).send({
-      message: "The user with the specified ID does not exist.",
-   });
 });
 
 app.listen(port, () => {
